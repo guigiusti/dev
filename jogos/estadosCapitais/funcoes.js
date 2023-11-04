a1c89a1c12b5bebce0d03e5fee20d655e7e3f04a
Guilherme <giusti.gui@gmail.com>
2023-11-04 00:09:11 -0300

    cria o jogo de estados e capitais

---
+++ jogos/estadosCapitais/funcoes.js
@@ -0,0 +1,43 @@
+var shownorhide = 0;
+
+// Esta primeira função faz carregar a imagem
+function selecionar_imagem(country_code) {
+    const img = document.querySelector("img"); 
+    img.src = '/guigiusti.github.io/dev/jogos/estadosCapitais/bandeirasEstados/svg/' + country_code + '.svg';
+    dados_do_pais(country_code); // Após carregar a imagem, chama a função para carregar os dados do país
+    return;
+}
+
+// Faz esconder o botão e faz mostrar os dados carregados, quando se aperta o botão HTML
+function showhide() {
+    document.getElementById('txt').hidden = false; // Faz o texto aparecer
+    document.getElementById('button_showhide_id').hidden = true; // Faz o botão sumir
+}
+
+//Procura nos arquivos JSON o nome do país e sua capital, em português
+function dados_do_pais(country_code) {
+    const pais = country_code.toUpperCase();
+    fetch('/guigiusti.github.io/dev/jogos/estadosCapitais/bandeirasEstados/estadosBR.json')
+    .then((response) => response.json())
+    .then((json) => {for (key in json) {if (json[key].sigla2 === pais) {criar_texto("Capital: "+json[key].capital,"capital");
+    return;}}})
+    fetch('/guigiusti.github.io/dev/jogos/estadosCapitais/bandeirasEstados/estadosBR.json')
+    .then((response) => response.json())
+    .then((json) => {for (key in json) {if (json[key].sigla2 === pais) {criar_texto("Estado: "+json[key].nome,"estado");
+    return;}}}) // A função criar_texto por fim é chamada para disponibilizar os dados encontrados
+}
+
+// Aqui os dados encontrados são transformados em elementos de texto
+// Continuando escondidos, esperando o pressionar do botão HTML para serem revelados
+function criar_texto (texto,id_texto) {
+    var texto_na_tela = document.createElement("h2")
+    var texto_na_tela_node = document.createTextNode(texto)
+    var texto_na_tela_space = document.createElement("br")
+    texto_na_tela.setAttribute("id",id_texto)
+    texto_na_tela.appendChild(texto_na_tela_node)
+    texto_na_tela.appendChild(texto_na_tela_space)
+    document.getElementById('txt').hidden = true
+    document.getElementById("txt").appendChild(texto_na_tela);
+    // if (urlParams.get('alpha2')) {showhide()} // Código de teste, faz automaticamente aparecer o texto (cont...)
+    // quando iniciado com o parâmetro "/?alpha2="
+}
---
+++ jogos/estadosCapitais/sketch.js
@@ -0,0 +1,21 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+// Para selecionar um país e bandeira especifica, use "/?sigla2=sp", substituindo "sp" pelo código desejado.
+// Os estados disponíveis estão no arquivo "estadosBR.json". Este código serve apenas para testes. 
+if (urlParams.get('sigla2')) {
+    fetch('/guigiusti.github.io/dev/jogos/estadosCapitais/bandeirasEstados/estadosBR.json')
+    .then((response) => response.json())
+    .then((json) => {const keys = Object.keys(json);
+        selecionar_imagem(urlParams.get('sigla2').toLowerCase());})
+}
+// Caso queira jogar, este é o código que rodará normalmente, escolhendo um país da lista de forma randômica. 
+// Neste caso, não é necessário inserir nada depois do "/"
+if (!urlParams.get('sigla2')) {
+fetch('/guigiusti.github.io/dev/jogos/estadosCapitais/bandeirasEstados/estadosBR.json')
+    .then((response) => response.json())
+    .then((json) => {const keys = Object.keys(json);
+        const len = keys.length;
+        const rnd = Math.floor(Math.random() * len);
+        selecionar_imagem(keys[rnd].toLowerCase());}) 
+        // Na última linha é enviado o comando com o código do país, dando início ao jogo.
+}
