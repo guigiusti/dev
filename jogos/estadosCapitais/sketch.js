54c6bee83247752e524c09c2f5c535c0cbbcba2b
Guilherme <giusti.gui@gmail.com>
2023-11-04 00:24:33 -0300

    atualiza o codigo

--- jogos/estadosCapitais/sketch.js
+++ jogos/estadosCapitais/sketch.js
@@ -6,7 +6,7 @@ if (urlParams.get('sigla2')) {
     fetch('/guigiusti.github.io/dev/jogos/estadosCapitais/bandeirasEstados/estadosBR.json')
     .then((response) => response.json())
     .then((json) => {const keys = Object.keys(json);
-        selecionar_imagem(urlParams.get('sigla2').toLowerCase());})
+        selecionar_imagem(urlParams.get('sigla2').toUpperCase());})
 }
 // Caso queira jogar, este é o código que rodará normalmente, escolhendo um país da lista de forma randômica. 
 // Neste caso, não é necessário inserir nada depois do "/"
@@ -16,6 +16,6 @@ fetch('/guigiusti.github.io/dev/jogos/estadosCapitais/bandeirasEstados/estadosBR
     .then((json) => {const keys = Object.keys(json);
         const len = keys.length;
         const rnd = Math.floor(Math.random() * len);
-        selecionar_imagem(keys[rnd].toLowerCase());}) 
+        selecionar_imagem(keys[rnd].toUpperCase());}) 
         // Na última linha é enviado o comando com o código do país, dando início ao jogo.
 }
