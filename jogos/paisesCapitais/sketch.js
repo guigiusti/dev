14f20882e9a69ad760631d99390dcba8b8441db3
Guilherme <giusti.gui@gmail.com>
2023-11-13 17:19:34 -0300

    atualiza o jogo para centralizar os jsons

--- jogos/paisesCapitais/sketch.js
+++ jogos/paisesCapitais/sketch.js
@@ -3,7 +3,7 @@ const urlParams = new URLSearchParams(queryString);
 // Para selecionar um país e bandeira especifica, use "/?alpha2=br", substituindo "br" pelo código desejado.
 // Os países disponíveis estão no arquivo "paisesUN.json". Este código serve apenas para testes. 
 if (urlParams.get('alpha2')) {
-    fetch('/guigiusti.github.io/dev/jogos/paisesCapitais/bandeirasPaises/paisesUN.json')
+    fetch('/guigiusti.github.io/dev/jogos/paisesCapitais/bandeirasPaises/paisesLista.json')
     .then((response) => response.json())
     .then((json) => {const keys = Object.keys(json);
         selecionar_imagem(urlParams.get('alpha2').toLowerCase());})
@@ -11,7 +11,7 @@ if (urlParams.get('alpha2')) {
 // Caso queira jogar, este é o código que rodará normalmente, escolhendo um país da lista de forma randômica. 
 // Neste caso, não é necessário inserir nada depois do "/"
 if (!urlParams.get('alpha2')) {
-fetch('/guigiusti.github.io/dev/jogos/paisesCapitais/bandeirasPaises/paisesUN.json')
+fetch('/guigiusti.github.io/dev/jogos/paisesCapitais/bandeirasPaises/paisesLista.json')
     .then((response) => response.json())
     .then((json) => {const keys = Object.keys(json);
         const len = keys.length;
