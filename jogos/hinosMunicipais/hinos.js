3ed2012d1184ad5daaae04baae583b63b2976326
Guilherme <giusti.gui@gmail.com>
2023-11-16 20:23:52 -0300

    stops using embed

--- jogos/hinosMunicipais/hinos.js
+++ jogos/hinosMunicipais/hinos.js
@@ -4,16 +4,7 @@ function arquivoCSV(){
     .then((json) => {const keys = Object.keys(json);
         const len = keys.length;
         const rnd = Math.floor(Math.random() * len);
-        selecionar_video(json[rnd][' link'])});}
-function selecionar_video(link) {
-    const link_yt = link
-    setTimeout(5)
-    console.log(link_yt)
-    const links = link_yt;
-    const urlParams = new URLSearchParams(links);
-    const link_send = "https://youtube.com/embed/" + urlParams.get('https://www.youtube.com/watch?v') + "?loop=1&controls=0&playlist=" + urlParams.get('https://www.youtube.com/watch?v');
-    atualiza_pagina(link_send)
-}
+        atualiza_pagina(json[rnd][' link'])});}
 function atualiza_pagina(link_recieve){
     document.getElementById("button_update").onclick = function () {
         window.open(link_recieve, "_blank");location.reload();
