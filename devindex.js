f5ec5080df25a3ae4726c987fa87361e1a65fb69
Guilherme <guilherme.romera@outlook.com>
2024-01-17 21:03:31 -0300

    updates files

--- devindex.js
+++ devindex.js
@@ -1,3 +1,2 @@
-window.location.pathname = "/"
-fetch('/guigiusti.github.io/textosbase/textos-index.html')
+fetch('/guigiusti.github.io/dev/dev.md')
     .then(r=>r.blob()).then(b=>b.text()).then(m=>{document.getElementById("devtxt").innerHTML=marked.parse(m)});
